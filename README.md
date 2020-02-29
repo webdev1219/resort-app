bce4ee8af0d38702381377c247cc58304eac5469
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2020-02-29 09:59:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Title
+
+## Motivation
+
+lorem ipsum
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+	res.render('user');
+});
+
+module.exports = router;
