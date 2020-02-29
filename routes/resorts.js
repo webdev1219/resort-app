412cfdc9bc246cce4463369632c3e7e2f8432cda
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2020-02-29 19:00:38 +0100

    add create and delete resorts

--- routes/resorts.js
+++ routes/resorts.js
@@ -3,16 +3,42 @@ const Resort = require('../models/Resort');
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res) => {
+/* GET resorts listing. */
+router.get('/', (req, res, next) => {
 	Resort.find()
 		.then(resorts => {
-			console.log('resorts', resorts);
 			res.render('resorts/list', {
 				resorts,
 			});
 		})
-		.catch(() => {});
+		.catch(next);
+});
+
+router.get('/add', (req, res) => {
+	res.render('resorts/create');
+});
+
+router.post('/', (req, res, next) => {
+	const { name, latitude, longitude } = req.body;
+	Resort.create({
+		name,
+		latitude: parseFloat(latitude),
+		longitude: parseFloat(longitude),
+	})
+		.then(() => {
+			res.redirect('/resorts');
+		})
+		.catch(next);
+});
+
+router.post('/:id/delete', (req, res, next) => {
+	const { id } = req.params;
+
+	Resort.findByIdAndDelete(id)
+		.then(() => {
+			res.redirect('/resorts');
+		})
+		.catch(next);
 });
 
 module.exports = router;
